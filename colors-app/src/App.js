f117fdc37766e178d8d8dcc98953a4a84c8f09b7
Jungir <googlman6300@gmail.com>
2019-08-01 17:45:06 +0600

    create react-app, palette.js, seedColor.js

---
+++ colors-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Palette from './Palette';
+import seedColor from './seedColor';
+
+function App() {
+  return (
+    <div >
+      <Palette {...seedColor[4]}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ colors-app/src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class palette extends Component {
+    render() {
+        return (
+            <div className='Palette'>
+                {/* Navbar goes here */}
+                <div className="Palette-colors">
+                    
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}

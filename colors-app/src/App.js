a6941ec589d7d36751e179d2e11089eced2544bb
Jungir <googlman6300@gmail.com>
2019-08-05 12:35:27 +0600

    fix color route

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
 import seedColor from './seedColor';
 import {generatePalette} from './colorHelpers';
-function App() {
-  
-  
-  return (
-    <Switch>
-      <Route exact path='/' render={() => <h1>Palete list goes here</h1>}/>
-      <Route exact path='/palette/:id' render={() => <h1>indiv palette goes here</h1>}/>
+class App extends Component {
 
-    </Switch>
-    // <div >
-      // <Palette palette={generatePalette(seedColor[4])}/>
-    // </div>
-  );
+  findPalette(id){
+    return seedColor.find(function (palette) {
+      return palette.id ===id;
+      });
+  }
+  render(){
+    return (
+      <Switch>
+        <Route exact path='/' render={() => <h1>Palete list goes here</h1>}/>
+        <Route exact path='/palette/:id' render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}/>
+      </Switch>
+      // <div >
+        // <Palette palette={generatePalette(seedColor[4])}/>
+      // </div>
+    );
+  }
 }
+  
+
+
+
 
 export default App;
